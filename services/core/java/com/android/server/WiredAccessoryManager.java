11ebb0bbffd2543f2fafc7b14c4f76a456b116fd
akodanka <anoob.anto.kodankandath@intel.com>
2020-06-24 13:12:52 +0530

    Handle simultaneous HDMI & WiredHeadphone events
    Wired Headset sets these 2 bits in the device when connected:
    SW_HEADPHONE_INSERT_BIT & SW_MICROPHONE_INSERT_BIT
    Additionally, if HDMI device is connected, it sets the
    SW_LINEOUT_INSERT_BIT event too.
    The combination of these 3 bits has to be handled correctly.
    
    Tracked-On: OAM-91511
    Signed-off-by: akodanka <anoob.anto.kodankandath@intel.com>
    Change-Id: If6f87502c15e1e34f9db7994830704cd07a79194

--- services/core/java/com/android/server/WiredAccessoryManager.java
+++ services/core/java/com/android/server/WiredAccessoryManager.java
@@ -520,11 +520,11 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
             //
             // If the kernel does not have an "hdmi_audio" switch, just fall back on the older
             // "hdmi" switch instead.
-            uei = new UEventInfo(NAME_HDMI_AUDIO, BIT_HDMI_AUDIO, 0, 0);
+            uei = new UEventInfo(NAME_HDMI_AUDIO, BIT_HDMI_AUDIO, BIT_LINEOUT, 0);
             if (uei.checkSwitchExists()) {
                 retVal.add(uei);
             } else {
-                uei = new UEventInfo(NAME_HDMI, BIT_HDMI_AUDIO, 0, 0);
+                uei = new UEventInfo(NAME_HDMI, BIT_HDMI_AUDIO, BIT_LINEOUT, 0);
                 if (uei.checkSwitchExists()) {
                     retVal.add(uei);
                 } else {
